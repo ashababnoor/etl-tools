743dd0d6be9d7dfc12de75aebcaca973f616d3dc
Shabab Noor <ashababnoor@gmail.com>
2023-12-06 23:22:43 +0600

    added functions for finding schemas and tables in postgres database

--- etl-tools/mysql-postgres-etl/src/finder.py
+++ etl-tools/mysql-postgres-etl/src/finder.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import psycopg2
 
 
 def find_mysql_dbs(host, user, password, database):
@@ -28,6 +29,42 @@ def find_mysql_tables(host, user, password, database):
     cursor = connection.cursor()
     
     cursor.execute("SHOW TABLES")
+    tables = [table[0] for table in cursor.fetchall()]
+    
+    cursor.close()
+    connection.close()
+    
+    return tables
+
+def find_postgres_schemas(host, port, user, password, database):
+    connection = psycopg2.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT schema_name FROM information_schema.schemata")
+    schemas = [schema[0] for schema in cursor.fetchall()]
+    
+    cursor.close()
+    connection.close()
+    
+    return schemas
+
+def find_postgres_tables(host, port, user, password, database):
+    connection = psycopg2.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
     tables = [table[0] for table in cursor.fetchall()]
     
     cursor.close()
